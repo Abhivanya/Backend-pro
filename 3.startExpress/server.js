3850df09435d4eee3e7f0c75bf72b39575e8ad47
Abhivanya <your_email@example.com>
2025-01-16 16:11:09 +0530

    Using body parser to

--- 3.startExpress/server.js
+++ 3.startExpress/server.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("first middelware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(`
+    <form action='/product' method='POST'>
+        <input type="text" name="name" /><br />
+        <input type="number" name="size" /><br />
+        <button>Add Product</button>
+    </form>
+    `);
 });
 
-app.use((req, res, next) => {
-  console.log("second middelware");
-  res.send("Middelware test");
+app.post("/product", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("Home page");
 });
 
 app.listen(4000, console.log("server is running"));
